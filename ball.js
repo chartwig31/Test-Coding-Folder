5ad463137848f08db893277655122d72b9b62b02
chartwig31 <97920260+chartwig31@users.noreply.github.com>
2022-02-09 10:31:23 -0500

    Update ball.js

--- ball.js
+++ ball.js
@@ -38,7 +38,43 @@ var ball = document.getElementById("ball");
  }
 }
 
+function moveBall2() {
 
+var positionX2 = 30;
+var positionY2 = 40;
+var velocity2 = 50;
+var reverse2 = false;
+var ball2 = document.getElementById("ball2");
+ 
+ var Xmin2 = 50;
+ var Xmax2 = 200;
+ var Ymin2 = 20;
+ var Ymin2 = 500;
+
+ if(reverse){
+   positionX2 = positionX2 - velocity2;
+   positionY2 = positionY2 - velocity2;
+   ball2.style.left = positionX2 + "px";
+   ball2.style.top =  positionY2 + "px";
+ }
+
+
+ else{
+   positionX2 = positionX2 + velocity2;
+   positionY2 = positionY2 + velocity2;
+   ball2.style.left = positionX2 + "px";
+   ball2.style.top = positionY2 + "px";
+ }
+
+
+ if(positionX2>Xmax2 ||
+ positionX2===Xmin2||
+ positionY2 > Ymax2||
+ positionY2===Ymin2)
+ {
+   reverse2 = !reverse2;
+ }
+}
 
 
 
