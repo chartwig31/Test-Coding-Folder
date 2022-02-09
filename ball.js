0d57670c288f75453cf8d84ab7ee60947eae57d0
chartwig31 <97920260+chartwig31@users.noreply.github.com>
2022-02-09 10:44:18 -0500

    Update ball.js

--- ball.js
+++ ball.js
@@ -1,86 +1,55 @@
 
+var maxWidth = 1000;
+var ballRadius = 25;
+
+// Ball 1
+var velocity = 7;
+var position = 0;
+var ball = document.getElementById('ball');
 
 function moveBall() {
-
-var positionX = 0;
-var positionY = 0;
-var velocity = 100;
-var reverse = false;
-var ball = document.getElementById("ball");
- 
- var Xmin = 0;
- var Xmax = 300;
- var Ymin = 0;
- var Ymin = 300;
-
- if(reverse){
-   positionX = positionX - velocity;
-   positionY = positionY - velocity;
-   ball.style.left = positionX + "px";
-   ball.style.top =  positionY + "px";
- }
-
-
- else{
-   positionX = positionX + velocity;
-   positionY = positionY + velocity;
-   ball.style.left = positionX + "px";
-   ball.style.top = positionY + "px";
- }
-
-
- if(positionX>Xmax ||
- positionX===Xmin||
- positionY > Ymax||
- positionY===Ymin)
- {
-   reverse = !reverse;
- }
+  position = position + velocity;
+  ball.style.left = position + 'px';
+  if(position >= maxWidth || position <= 0) velocity=velocity*-1;
+  checkCollision(ball);
 }
+
+setInterval(moveBall, 10);
+
+// Ball 2
+var velocity2 = 5;
+var position2 = 0;
+var ball2 = document.getElementById('ball2');
 
 function moveBall2() {
-
-var positionX2 = 30;
-var positionY2 = 40;
-var velocity2 = 50;
-var ball2 = document.getElementById("ball2");
- 
- var Xmin2 = 50;
- var Xmax2 = 200;
- var Ymin2 = 20;
- var Ymin2 = 500;
-
- if(reverse){
-   positionX2 = positionX2 - velocity2;
-   positionY2 = positionY2 - velocity2;
-   ball2.style.left = positionX2 + "px";
-   ball2.style.top =  positionY2 + "px";
- }
-
-
- else{
-   positionX2 = positionX2 + velocity2;
-   positionY2 = positionY2 + velocity2;
-   ball2.style.left = positionX2 + "px";
-   ball2.style.top = positionY2 + "px";
- }
-
-
- if(positionX2>Xmax2 ||
- positionX2===Xmin2||
- positionY2 > Ymax2||
- positionY2===Ymin2)
- {
-   reverse = !reverse;
- }
+  position2 = position2 + velocity2;
+  ball2.style.left = position2 + 'px';
+  if(position2 >= maxWidth || position2 <= 0) velocity2=velocity2*-1;
+  checkCollision(ball2);
 }
 
+setInterval(moveBall2, 10);
+
+// Ball 3
+var velocity3 = 4;
+var position3 = 0;
+var ball3 = document.getElementById('ball3');
+
+function moveBall3() {
+  position3 = position3 + velocity3;
+  ball3.style.left = position3 + 'px';
+  if(position3 >= maxWidth || position3 <= 0) velocity3=velocity3*-1;
+  checkCollision(ball3);
+}
+
+setInterval(moveBall3, 10);
 
 
 
 
-// This call the moveBall function every 100ms
 
 
-setInterval(moveBall, 100);
-setInterval(moveBall2, 50);
+
+
+
+
